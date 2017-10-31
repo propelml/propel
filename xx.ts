0d2c63f9562f4f5abe2bb63af2b9b4d6f29b2809
Ryan Dahl <ry@tinyclouds.org>
2017-10-31 11:28:28 -0400

    Make tanh forward pass work. Add assert functions.

--- xx.ts
+++ xx.ts
@@ -5,44 +5,54 @@ import {NDArrayMathCPU} from './deeplearnjs/src/math/math_cpu';
 
 let math = new NDArrayMathCPU();
 
-function toArray(x: number|NDArray) {
+function toNDArray(x: number|NDArray) {
   if (typeof x == "number") {
     return NDArray.make([], {values: new Float32Array([x])});
   }
   return x;
 }
 
+export function assert(expr: boolean, msg: string) {
+  if (!expr) {
+    throw new Error(msg);
+  }
+}
+
+export function assertClose(a: number, b: number, msg: string) {
+  assert(Math.abs(a - b) < 0.001, msg);
+}
+
 export function exp(a) {
-  a = toArray(a);
-  return math.relu(a);
+  a = toNDArray(a);
+  return math.exp(a);
 }
 
 export function neg(a) {
-  a = toArray(a);
-  return a;
+  a = toNDArray(a);
+  return math.neg(a);
 }
 
 export function add(a, b) {
-  a = toArray(a);
-  b = toArray(b);
+  a = toNDArray(a);
+  b = toNDArray(b);
   return math.add(a, b);
 }
 
 export function sub(a, b) {
-  a = toArray(a);
-  b = toArray(b);
+  a = toNDArray(a);
+  b = toNDArray(b);
   return add(a, neg(b));
 }
 
 export function div(a, b) {
-  a = toArray(a);
-  b = toArray(b);
+  a = toNDArray(a);
+  b = toNDArray(b);
   return math.divide(a, b);
 }
 
 export function mul(a, b) {
-  a = toArray(a);
-  b = toArray(b);
+  a = toNDArray(a);
+  b = toNDArray(b);
   return math.multiply(a, b);
 }
 
