4d9272ef1177834f8d49031854effc4f8cba89f1
Ryan Dahl <ry@tinyclouds.org>
2017-10-31 11:28:28 -0400

    Make tanh forward pass work. Add assert functions.

--- test.ts
+++ test.ts
@@ -5,6 +5,9 @@ function tanh(x) {
   return xx.div(xx.sub(1.0, y), xx.add(1.0, y));
 }
 
+xx.assertClose(tanh(1.0).getValues()[0], 0.7615, "tanh broken");
+
+
 let gradTanh = xx.grad(tanh);
 let r = gradTanh(1.0)
 
