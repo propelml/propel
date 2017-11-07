ab1ede20be5a72f3ea2555d19e8c90da7e1aaa10
Ryan Dahl <ry@tinyclouds.org>
2017-11-07 04:14:58 -0500

    Use webpack. Demo loading sigprop from index.html

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+var webpack = require('webpack');
+
+module.exports = {
+  entry: {
+    'sigprop': './sigprop.ts',
+    'sigprop.min': './sigprop.ts'
+  },
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin({
+      minimize: true,
+      sourceMap: true,
+      include: /\.min\.js$/,
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+      }
+    ]
+  },
+  devtool: 'source-map',
+  resolve: {
+    extensions: [ '.ts', '.js' ]
+  },
+  output: {
+    filename: '[name].js',
+    path: __dirname + '/dist',
+    libraryTarget: "var",
+    library: "sigprop"
+  }
+};
