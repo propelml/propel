7a4b6731286739abf0d31a5636a469424c02fbd7
Ryan Dahl <ry@tinyclouds.org>
2017-11-07 18:13:28 -0500

    Fix tensor_test.ts

--- tensor_test.ts
+++ tensor_test.ts
@@ -1,18 +1,18 @@
-import * as sp from './sigprop';
+import sp from './sigprop';
 import {assertEqual, assertAllEqual} from './util';
 
 function testShapes() {
-  let a = new sp.Tensor([[1, 2], [3, 4]]);
+  let a = sp([[1, 2], [3, 4]]);
   assertAllEqual(a.shape, [2, 2]);
 
-  let b = new sp.Tensor([[[1, 2]]]);
+  let b = sp([[[1, 2]]]);
   assertAllEqual(b.shape, [1, 1, 2]);
 }
 
 function testMul() {
-  let a = new sp.Tensor([[1, 2], [3, 4]]);
-  let expected = new sp.Tensor([[1, 4], [9, 16]]);
-  let actual = sp.mul(a, a);
+  let a = sp([[1, 2], [3, 4]]);
+  let expected = sp([[1, 4], [9, 16]]);
+  let actual = a.mul(a);
   assertAllEqual(actual.shape, [2, 2]);
   assertAllEqual(actual, expected);
 }
