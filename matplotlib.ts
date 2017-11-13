93146a411b6662146ab686bba8acd14dfb5f89cf
Ryan Dahl <ry@tinyclouds.org>
2017-11-13 17:31:24 -0500

    Add HTML REPL
    To use, run webpack, then open repl.html. Shift+Enter will evaluate a cell.

---
+++ matplotlib.ts
@@ -0,0 +1,93 @@
+import * as repl from "./repl";
+import * as d3 from "d3";
+import sp from "./sigprop";
+import {assertEqual} from "./util";
+
+let currentPlot = null;
+
+// TODO colors should match those used in highlight.js.
+let color = d3.scaleOrdinal(d3.schemeCategory10);
+
+
+function plotLines(data) {
+  var outputId = repl.outputId();
+  // Make an SVG Container
+  let svg = d3.select(outputId).append("svg")
+    .attr("width", 400)
+    .attr("height", 200);
+  let margin = {top: 10, right: 10, bottom: 10, left: 10};
+  let width = +svg.attr("width") - margin.left - margin.right;
+  let height = +svg.attr("height") - margin.top - margin.bottom;
+  let g = svg.append("g")
+    .attr("transform", `translate(${margin.left},${margin.top})`);
+
+  var xScale = d3.scaleLinear()
+    .domain([-7, 7])
+    .range([0, width]);
+
+  var yScale = d3.scaleLinear()
+    .domain([-1, 1])
+    .range([height, 0]);
+
+  var line = (d3.line() as any)
+    .x(d => xScale(d.x))
+    .y(d => yScale(d.y))
+
+  g.selectAll('path').data(data)
+    .enter()
+    .append("path")
+    .attr("d", <any>line)
+        .style("fill", "none" )
+        .style("stroke-width", "2px" )
+        .style("stroke", (d, i) => {
+          return color(<any>i);
+        })
+}
+
+function plot(...args) {
+  let xs = [];
+  let ys = [];
+  let state = "x";
+  for (let i = 0; i < args.length; i++) {
+    let arg = args[i];
+    switch (state) {
+        case "x":
+          xs.push(arg);
+          state = "y";
+          break;
+
+        case "y":
+          ys.push(arg);
+          state = "x";
+          break;
+
+    }
+  }
+
+  assertEqual(xs.length, ys.length);
+  let data = [];
+  for (let i = 0; i < xs.length; ++i) {
+    // TODO line = sp.stack([xs[i], ys[i]], 1)
+    let xv = xs[i].ndarray.getValues();
+    let yv = ys[i].ndarray.getValues();
+    assertEqual(xv.length, yv.length);
+    let line = [];
+    for (let j = 0; j < xv.length; ++j) {
+      line.push({x: xv[j], y: yv[j]});
+    }
+    data.push(line);
+  }
+
+  plotLines(data);
+  //currentPlot = svg;
+  //return svg
+}
+
+function show(...args) {
+  //repl.appendOutput(currentPlot);
+}
+
+export default {
+  "plot": plot,
+  "show": show,
+}
---
+++ repl.ts
@@ -0,0 +1,198 @@
+import * as hljs from "highlight.js";
+
+import sigprop from "./sigprop";
+import matplotlib from "./matplotlib";
+
+let cellsElement = document.createElement('div');
+let _log = console.log;
+// If you use the eval function indirectly, by invoking it via a reference
+// other than eval, as of ECMAScript 5 it works in the global scope rather than
+// the local scope. This means, for instance, that function declarations create
+// global functions, and that the code being evaluated doesn't have access to
+// local variables within the scope where it's being called.
+let globalEval = eval;
+
+let _REPLAppendOutput = null;
+let _REPLOutputId = null;
+
+export function appendOutput(svg) {
+  _REPLAppendOutput(svg);
+}
+
+export function outputId(): string {
+  return _REPLOutputId();
+}
+
+window["define"] = function define(x) {
+  _log("define", x);
+}
+
+window["require"] = function require(target) {
+  //_log("require", target);
+  let m = {
+    "sigprop": sigprop,
+    "matplotlib": matplotlib
+  }[target];
+  if (m) {
+    return m;
+  }
+  throw new Error("Unknown module: " + target);
+}
+
+window["exports"] = {};
+
+// TODO Yes, extremely hacky.
+function transpile(source: string): string {
+  return source.replace(/import (\w+) from ("[^"]+"|'[^']+')/g, (m, p1, p2) => {
+    let x = `${p1} = require(${p2})`;
+    //_log("replace", p1, p2, x);
+    return x;
+  });
+}
+
+class Cell {
+  isLoad: boolean;
+  script: HTMLScriptElement;
+  output: HTMLElement;
+  input: HTMLElement;
+  id: number;
+  static nextId = 1;
+
+  constructor(script?: HTMLScriptElement) {
+    this.id = Cell.nextId++;
+    this.script = script;
+    //_log("create cell");
+
+    let input = document.createElement("pre");
+    input.style.padding = "10px";
+    this.input = input;
+    input.onkeydown = this.onkeydown.bind(this);
+    cellsElement.appendChild(input);
+
+    if (this.script.src) {
+      input.innerText = `<script src="${this.script.src}"/>`;
+      input.className = "language-html";
+      this.isLoad = true;
+    } else {
+      input.innerText = this.script.innerText.replace(/^\s+/, '');
+      input.className = "language-javascript";
+      input.contentEditable = "true";
+      this.isLoad = false;
+    }
+    hljs.highlightBlock(input);
+
+    this.output = document.createElement("div");
+    this.output.id = `output${this.id}`;
+    this.output.style.padding = "0 10px";
+    this.output.style["font-family"] = "monospace";
+    this.output.style["white-space"] = "pre-wrap";
+    cellsElement.appendChild(this.output);
+  }
+
+  onkeydown(e) {
+    if (e.which == 13 && e.shiftKey) {
+      _log("return shift");
+      hljs.highlightBlock(this.input); // rehighlight.
+      this.output.innerText = ""; // Clear output.
+      this.execute();
+      return false;
+    }
+  }
+
+  log(...args) {
+    // messy
+    let s = args.map(a => a.toString()).join(" ");
+    let last = this.output.lastChild; 
+    if (last && last.nodeType != Node.TEXT_NODE) {
+      s = "\n" + s;
+    }
+    var t = document.createTextNode(s + "\n");
+    this.output.appendChild(t);
+    _log(...args);
+  }
+
+  error(...args) {
+    // messy
+    let s = args.map(a => a.toString()).join(" ");
+    let last = this.output.lastChild; 
+    if (last && last.nodeType != Node.TEXT_NODE) {
+      s = "\n" + s;
+    }
+    var t = document.createElement("b");
+    t.innerText = s + "\n";
+    this.output.appendChild(t);
+    _log(...args);
+  }
+
+  appendOutput(svg) {
+    this.output.appendChild(svg);
+  }
+
+  outputId(): string {
+    return "#" + this.output.id;
+  }
+
+  execute(done = null) {
+    console.log = this.log.bind(this);
+    _REPLAppendOutput = this.appendOutput.bind(this);
+    _REPLOutputId = this.outputId.bind(this);
+    this.script.remove();
+    this.script['type'] = "text/javascript";
+    if (this.isLoad) {
+      _log("execute", this.script.src);
+      document.head.appendChild(this.script);
+      this.script.onload = done;
+    } else {
+      let source = this.script.innerText = this.input.innerText;
+
+      let jsSource = transpile(source);
+
+      let rval = undefined;
+      try {
+        rval = globalEval(jsSource);
+      } catch(e) {
+        this.error(e.stack);
+      }
+      if (rval) this.log(rval);
+      if (done) done();
+    }
+  }
+}
+
+function createNewCellButton() {
+  let b = document.createElement("button");
+  b.innerText = "New Cell";
+  b.style.margin = "50px 10px";
+  document.body.appendChild(b);
+  b.onclick = () => {
+    _log("Button click");
+    let s = document.createElement('script');
+    s['type'] = 'repl';
+    new Cell(s);
+    window.scrollBy(0, 500); // scroll down.
+  };
+}
+
+window.onload = () => {
+  document.body.style.margin = "0";
+  document.body.appendChild(cellsElement);
+
+  createNewCellButton();
+
+  let cells = [];
+  for (let i = 0; i < document.scripts.length; ++i) {
+    let s = document.scripts[i];
+    if (s.type != "repl") continue;
+    cells.push(new Cell(s));
+  }
+
+  let execCounter = 0;
+  function execNext() {
+    if (execCounter < cells.length) {
+      let cell = cells[execCounter++];
+      cell.execute(execNext);
+    }
+  }
+  execNext();
+};
+
