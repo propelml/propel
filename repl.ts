cb47c8be157592a84eab72baafc3063fc2071e5d
Ryan Dahl <ry@tinyclouds.org>
2017-11-18 12:22:24 -0500

    Refactor notebook.html to use CodeMirror.

--- repl.ts
+++ repl.ts
@@ -1,9 +1,10 @@
-import * as hljs from "highlight.js";
+import * as CodeMirror from "codemirror";
+import "codemirror/mode/javascript/javascript.js";
 
 import propel from "./propel";
 import matplotlib from "./matplotlib";
 
-let cellsElement = document.createElement('div');
+let cellsElement = null;
 let _log = console.log;
 // If you use the eval function indirectly, by invoking it via a reference
 // other than eval, as of ECMAScript 5 it works in the global scope rather than
@@ -48,51 +49,34 @@ function transpile(source: string): string {
 
 class Cell {
   isLoad: boolean;
-  script: HTMLScriptElement;
   output: HTMLElement;
-  input: HTMLElement;
+  editor: CodeMirror.Editor;
   id: number;
   static nextId = 1;
 
-  constructor(script?: HTMLScriptElement) {
+  constructor(source?: string) {
     this.id = Cell.nextId++;
-    this.script = script;
-    //_log("create cell");
 
-    let input = document.createElement("pre");
-    input.style.padding = "10px";
-    this.input = input;
-    input.onkeydown = this.onkeydown.bind(this);
-    cellsElement.appendChild(input);
-
-    if (this.script.src) {
-      input.innerText = `<script src="${this.script.src}"/>`;
-      input.className = "language-html";
-      this.isLoad = true;
-    } else {
-      input.innerText = this.script.innerText.replace(/^\s+/, '');
-      input.className = "language-javascript";
-      input.contentEditable = "true";
-      this.isLoad = false;
-    }
-    hljs.highlightBlock(input);
+    this.editor = CodeMirror(cellsElement, {
+      value: source ? source.trim() : "",
+      lineNumbers: false,
+      viewportMargin: Infinity
+    });
+    this.editor.setOption("extraKeys", {
+      "Shift-Enter": this.onShiftEnter.bind(this)
+    });
 
     this.output = document.createElement("div");
+    this.output.className = 'output';
     this.output.id = `output${this.id}`;
-    this.output.style.padding = "0 10px";
-    this.output.style["font-family"] = "monospace";
-    this.output.style["white-space"] = "pre-wrap";
     cellsElement.appendChild(this.output);
   }
 
-  onkeydown(e) {
-    if (e.which == 13 && e.shiftKey) {
-      _log("return shift");
-      hljs.highlightBlock(this.input); // rehighlight.
-      this.output.innerText = ""; // Clear output.
-      this.execute();
-      return false;
-    }
+  onShiftEnter(cm) {
+     _log("ShiftEnter");
+    this.output.innerText = ""; // Clear output.
+    this.execute();
+    return false;
   }
 
   log(...args) {
@@ -132,54 +116,40 @@ class Cell {
     console.log = this.log.bind(this);
     _REPLAppendOutput = this.appendOutput.bind(this);
     _REPLOutputId = this.outputId.bind(this);
-    this.script.remove();
-    this.script['type'] = "text/javascript";
-    if (this.isLoad) {
-      _log("execute", this.script.src);
-      document.head.appendChild(this.script);
-      this.script.onload = done;
-    } else {
-      let source = this.script.innerText = this.input.innerText;
-
-      let jsSource = transpile(source);
-
-      let rval = undefined;
-      try {
-        rval = globalEval(jsSource);
-      } catch (e) {
-        this.error(e.stack);
-      }
-      if (rval) this.log(rval);
-      if (done) done();
+    let source = transpile(this.editor.getValue());
+    let rval = undefined;
+    try {
+      rval = globalEval(source);
+    } catch (e) {
+      this.error(e.stack);
     }
+    if (rval) this.log(rval);
+    if (done) done();
   }
 }
 
-function createNewCellButton() {
-  let b = document.createElement("button");
-  b.innerText = "New Cell";
-  b.style.margin = "50px 10px";
-  document.body.appendChild(b);
-  b.onclick = () => {
-    _log("Button click");
-    let s = document.createElement('script');
-    s['type'] = 'repl';
-    new Cell(s);
-    window.scrollBy(0, 500); // scroll down.
-  };
+function newCellClick() {
+  _log("Button click");
+  new Cell();
+  window.scrollBy(0, 500); // scroll down.
 }
 
 window.onload = () => {
-  document.body.style.margin = "0";
-  document.body.appendChild(cellsElement);
+  cellsElement = document.getElementById('cells');
+  document.getElementById('newCell').onclick = newCellClick;
 
-  createNewCellButton();
+  // Pre-existing cells are stored as <script type=repl> elements.
+  // These script tags are promptly removed from the DOM and
+  // replaced with CodeMirror-handled textareas containing
+  // the source code. We use <script type=repl> in order to get
+  // proper syntax highlighting in editors.
 
   let cells = [];
-  for (let i = 0; i < document.scripts.length; ++i) {
-    let s = document.scripts[i];
-    if (s.type != "repl") continue;
-    cells.push(new Cell(s));
+  let replScripts = Array.from(document.scripts).filter(
+    s => s.type == 'repl');
+  for (let s of replScripts) {
+    s.remove();
+    cells.push(new Cell(s.innerText));
   }
 
   let execCounter = 0;
