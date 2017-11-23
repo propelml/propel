75c73248faf8ff4340421b15d6ccf79bdd86f2a5
Ryan Dahl <ry@tinyclouds.org>
2017-11-22 16:22:47 -0800

    travis.yml to use tools/test.sh

--- tools/test.sh
+++ tools/test.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
+set -e -v
 cd `dirname "$0"`; cd ..
-set -e
-./tools/webpack
-ts-node backprop_test.ts
-ts-node tensor_test.ts
-ts-node util_test.ts
-node test-tensorflow-binding.js
+# GYP normally hides the exact commands being run unless V=1 is set.
+export V=1
+./deps/cpplint/cpplint.py *.cc
+npm build .
+node ./node_modules/webpack/bin/webpack.js
+./node_modules/typescript/bin/tsc
+node dist/backprop_test.js
+node dist/tensor_test.js
+node dist/util_test.js
+node binding_test.js
 echo PASS
