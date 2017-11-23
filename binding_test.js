2ac42bbe67c147cf06f59cec4be9b2fd8f9b701d
Ryan Dahl <ry@tinyclouds.org>
2017-11-22 16:13:56 -0800

    First pass at TFE_Context binding.

--- binding_test.js
+++ binding_test.js
@@ -5,6 +5,7 @@ try {
 } catch (e) {
   tf = require('./build/Release/tensorflow-binding.node');
 }
+let ctx = new tf.Context();
 
 let typedArray = new Uint16Array([1, 2, 3, 4, 5, 6]);
 let tensor = new tf.Tensor(typedArray, [2, 3]);
