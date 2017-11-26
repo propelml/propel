c91fabf8ab6ee721d859553f2a944ab1a0535ecc
Ryan Dahl <ry@tinyclouds.org>
2017-11-26 09:23:41 -0800

    Call tslint during CI.

--- tools/presubmit.sh
+++ tools/presubmit.sh
@@ -3,6 +3,7 @@ set -e -v
 cd `dirname "$0"`; cd ..
 # GYP normally hides the exact commands being run unless V=1 is set.
 export V=1
+./tools/tslint.sh
 ./deps/cpplint/cpplint.py *.cc
 npm build .
 node ./node_modules/webpack/bin/webpack.js
