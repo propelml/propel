a004c6eed2241384bbafb18f8091c960f534a9fe
Bert Belder <bertbelder@gmail.com>
2017-12-09 18:55:55 +0100

    api: export the actual Tensor constructor

--- api.ts
+++ api.ts
@@ -1,10 +1,10 @@
 import * as backprop from "./backprop";
 import { basicOps } from "./basic";
 export { backend } from "./basic";
-import { ChainableTensor, convertChainable } from "./chainable_tensor";
+import { ChainableTensor as Tensor, convertChainable }
+  from "./chainable_tensor";
+export { ChainableTensor as Tensor } from "./chainable_tensor";
 import * as types from "./types";
-
-export type Tensor = ChainableTensor;
 
 export function $(t: types.TensorLike): Tensor {
   return convertChainable(t);
@@ -15,14 +15,14 @@ export const multigrad = backprop.multigrad;
 
 export function eye(size: number, dtype: types.DType = "float32"): Tensor {
   const t = basicOps.eye(size, dtype);
-  return new ChainableTensor(t);
+  return new Tensor(t);
 }
 
 // Returns num evenly spaced samples, calculated over the interval
 // [start, stop].
 export const linspace = (start: number, stop: number, num = 50): Tensor => {
   const t = basicOps.linspace(start, stop, num);
-  return new ChainableTensor(t);
+  return new Tensor(t);
 };
 
 // Return evenly spaced numbers over a specified interval.
@@ -51,7 +51,7 @@ export const arange = function(...args: number[]): Tensor {
       throw new Error("Bad number of arguments.");
   }
   const t = basicOps.arange(start, limit, delta);
-  return new ChainableTensor(t);
+  return new Tensor(t);
 };
 
 export const square = (x) => $(x).square();
@@ -61,7 +61,7 @@ export const tanh = (x) => $(x).tanh();
 
 export function randn(...shape: number[]): Tensor {
   const t = basicOps.randn(shape);
-  return new ChainableTensor(t);
+  return new Tensor(t);
 }
 
 export const matmul = (x, y) => $(x).matmul(y);
