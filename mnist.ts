312f952b910691d9e7f1053c194be4a4617ad4d2
Ryan Dahl <ry@tinyclouds.org>
2017-12-12 12:46:23 -0500

    Add mnist library.

---
+++ mnist.ts
@@ -0,0 +1,98 @@
+// TODO This file is Node specific. Adapt for web.
+import { existsSync, readFileSync } from "fs";
+import { basename, resolve } from "path";
+import { $ } from "./api";
+import { assert, assertEqual, log } from "./util";
+
+// If compiled to JS, this might be in a different directory.
+export const dirname = basename(__dirname) === "dist" ?
+  resolve(__dirname, "../deps/mnist") :
+  resolve(__dirname, "deps/mnist");
+
+export function filenames(split: string): [string, string] {
+  if (split === "train") {
+    return [
+      resolve(dirname, "train-labels-idx1-ubyte"),
+      resolve(dirname, "train-images-idx3-ubyte"),
+    ];
+  } else if (split === "test") {
+    return [
+      resolve(dirname, "t10k-labels-idx1-ubyte"),
+      resolve(dirname, "t10k-images-idx3-ubyte"),
+    ];
+  } else {
+    assert(false, "Bad split: " + split);
+  }
+}
+
+function littleEndianToBig(val) {
+    return ((val & 0x00FF) << 24) |
+           ((val & 0xFF00) << 8) |
+           ((val >> 8) & 0xFF00) |
+           ((val >> 24) & 0x00FF);
+}
+
+function bufferToTypedArray(b: any): [Int32Array, Uint8Array] {
+  // TODO ensure zero copy if possible.
+  // var ab = b.buffer.slice(b.byteOffset, b.byteOffset + b.byteLength);
+  const i32 = new Int32Array(b.buffer, b.byteOffset,
+                           b.byteLength / Int32Array.BYTES_PER_ELEMENT);
+  const ui8 = new Uint8Array(b.buffer, b.byteOffset, b.byteLength);
+  return [i32, ui8];
+}
+
+// TODO Remove once pretty printing lands.
+function inspectImg(t, idx) {
+  const img = t.slice([idx, 0, 0], [1, -1, -1]);
+  console.log("img");
+  const imgData = img.getData();
+  let s = "";
+  for (let j = 0; j < 28 * 28; j++) {
+    s += imgData[j].toString() + " ";
+    if (j % 28 === 27) s += "\n";
+  }
+  console.log(s);
+}
+
+function loadFile(fn, split, images: boolean) {
+  const fileBuffer = readFileSync(fn, null);
+  const [i32, ui8] = bufferToTypedArray(fileBuffer);
+  const magicValue = images ? 2051 : 2049;
+  const numExamples = split === "train" ? 60000 : 10000;
+  let i = 0;
+  assertEqual(littleEndianToBig(i32[i++]), magicValue);
+  assertEqual(littleEndianToBig(i32[i++]), numExamples);
+  if (images) {
+    assertEqual(littleEndianToBig(i32[i++]), 28);
+    assertEqual(littleEndianToBig(i32[i++]), 28);
+  }
+  const tensorData = ui8.slice(4 * i) as Uint8Array;
+  let t = $(tensorData, "uint8");
+  if (images) {
+    t = t.reshape([numExamples, 28, 28]);
+  } else {
+    t = t.reshape([numExamples]);
+  }
+  return t;
+}
+
+export function load(split: string, batchSize: number) {
+  assert(existsSync(dirname));
+  const [labelFn, imageFn] = filenames(split);
+  const images = loadFile(imageFn, split, true);
+  const labels = loadFile(labelFn, split, false);
+  // inspectImg(images, 7);
+  let idx = 0;
+  return {
+    next: () => {
+      // Wrap around.
+      if (idx + batchSize >= images.shape[0]) {
+        idx = 0;
+      }
+      const imageBatch = images.slice([idx, 0, 0], [batchSize, -1, -1]);
+      const labelBatch = labels.slice([idx], [batchSize]);
+      idx += batchSize;
+      return [imageBatch, labelBatch];
+    }
+  };
+}
---
+++ mnist_test.ts
@@ -0,0 +1,26 @@
+import * as mnist from "./mnist";
+import { assert, assertAllEqual, assertShapesEqual } from "./util";
+
+function testTrainSplit() {
+  const dataset = mnist.load("train", 256);
+  const [images, labels] = dataset.next();
+  assertShapesEqual(images.shape, [256, 28, 28]);
+  console.log("images dtype", images.dtype);
+  assert(images.dtype === "uint8");
+  assertShapesEqual(labels.shape, [256]);
+  assert(labels.dtype === "uint8");
+  assertAllEqual(labels.slice([0], [3]), [5, 0, 4]);
+}
+
+function testTestSplit() {
+  const dataset = mnist.load("test", 16);
+  const [images, labels] = dataset.next();
+  assertShapesEqual(images.shape, [16, 28, 28]);
+  assert(images.dtype === "uint8");
+  assertShapesEqual(labels.shape, [16]);
+  assert(labels.dtype === "uint8");
+  assertAllEqual(labels.slice([0], [3]), [7, 2, 1]);
+}
+
+testTrainSplit();
+testTestSplit();
