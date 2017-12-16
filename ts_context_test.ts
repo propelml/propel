7cf1f6a489cca562831805c89b7fba4db3f40eaa
Bert Belder <bertbelder@gmail.com>
2017-12-16 23:37:41 +0100

    ts-context: make it able to load .js files

--- ts_context_test.ts
+++ ts_context_test.ts
@@ -1,5 +1,18 @@
 import * as assert from "assert";
-import { Context } from "./ts_context";
+import { Context, fileext } from "./ts_context";
+
+assert.strictEqual(fileext(""), "");
+assert.strictEqual(fileext(".dotfile"), "dotfile");
+assert.strictEqual(fileext("a/dir.weird/.dotfile"), "dotfile");
+assert.strictEqual(fileext("a/dir.weird/file"), "");
+assert.strictEqual(fileext("a/dir.weird/file"), "");
+assert.strictEqual(fileext("a/dir.weird/file.ext"), "ext");
+assert.strictEqual(fileext("a/dir/"), "");
+assert.strictEqual(fileext("file"), "");
+assert.strictEqual(fileext("file."), "");
+assert.strictEqual(fileext("file.ext"), "ext");
+assert.strictEqual(fileext("file.foo."), "");
+assert.strictEqual(fileext("file.foo.bar"), "bar");
 
 const ctx = new Context();
 
@@ -27,18 +40,20 @@ async function fail(code) {
   await pass("Math.PI", Math.PI);
   await pass("console.log('hi')");
   await pass("console.log('one');console.log('two');42");
+  await fail("import * as x from 'does not exist'; x");
   await pass("import * as fs from 'fs'; void fs");
   await pass(`import * as jquery from
               'https://code.jquery.com/jquery-3.2.1.slim.min.js'; jquery`);
   await pass("typeof jquery", "function");
-  // await pass("import * as u from './util'; u"); -- TODO: add .js ext
+  await pass("import * as u from './util'; u");
   await pass(`// Multi-line inputs are supported.
               (async () => {
                 console.log('In here!');
                 await new Promise((res, rej) => setTimeout(res, 1));
               })();`);
   await pass("let s: string = '44'; s", "44");
-  await pass("import * as dl from './dl'");
+  await pass("import * as dl from './dl'; dl");
+  await pass("import * as ut from './util_test'; ut");
   // await fail("const a; const a;"); -- TODO: enable type checking.
   await fail("throw new Error('wut')");
   await fail("sdfk.gjhsdk;lfghskl,dfjgkl");
