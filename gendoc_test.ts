d1f7471c47e694f4b7857f96b2b7ab577419d1b9
Ryan Dahl <ry@tinyclouds.org>
2017-12-26 23:03:47 -0500

    Support live examples in docs.

--- gendoc_test.ts
+++ gendoc_test.ts
@@ -1,7 +1,7 @@
 import * as gendoc from "./gendoc";
 import { assert } from "./util";
 
-function testBasic() {
+function testSmoke() {
   const docs = gendoc.genJSON();
   assert(docs.length > 5);
   assert(docs.map(e => e.name).indexOf("Tensor") >= 0);
@@ -9,4 +9,52 @@ function testBasic() {
   assert(html.length > 0);
 }
 
-testBasic();
+function testMarkupDocStr() {
+  const docstr = [
+    "hello",
+    "",
+    "  x = 1 + 2;",
+    "",
+    "world",
+  ].join("\n");
+  const actual = gendoc.markupDocStr(docstr);
+  const expected = [
+    "hello",
+    "",
+    "<script type=notebook>",
+    "x = 1 + 2;",
+    "</script>",
+    "",
+    "world",
+  ].join("\n");
+  // console.log("actual", JSON.stringify(actual));
+  // console.log("expected", JSON.stringify(expected));
+  assert(actual === expected);
+}
+
+function testMarkupDocStr2() {
+  const docstr = [
+    "like this:",
+    "",
+    "  params.forEach((tensor, name) => {",
+    "    console.log(tensor);",
+    "  });",
+  ].join("\n");
+  const actual = gendoc.markupDocStr(docstr);
+  const expected = [
+    "like this:",
+    "",
+    "<script type=notebook>",
+    "params.forEach((tensor, name) => {",
+    "  console.log(tensor);",
+    "});",
+    "</script>",
+  ].join("\n");
+  // console.log("actual", JSON.stringify(actual));
+  // console.log("expected", JSON.stringify(expected));
+  assert(actual === expected);
+}
+
+testSmoke();
+testMarkupDocStr();
+testMarkupDocStr2();
