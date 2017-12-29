f47a8de9ef5db5b8f29a50dbec017c7f832144d4
Ryan Dahl <ry@tinyclouds.org>
2017-12-28 20:14:18 -0500

    Fork flatten and inferShape into our source code.
    This fixes the circular dependeny issue described in a0308f and 83d241.

--- util.ts
+++ util.ts
@@ -1,4 +1,3 @@
-import { flatten, inferShape } from "./deps/deeplearnjs/src/util";
 import { Tensor } from "./tensor";
 import { BasicTensor, FlatVector, isTypedArray, RegularArray, Shape,
   TensorLike } from "./types";
@@ -174,4 +173,45 @@ export function bcastGradientArgs(sx: Shape, sy: Shape): [Shape, Shape] {
     }
   }
   return [rx, ry];
+}
+
+/**
+ * flatten() and inferShape were forked from deps/deeplearnjs/src/util.ts
+ *
+ * Copyright 2017 Google Inc. All Rights Reserved.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * =============================================================================
+ */
+
+export function flatten(
+    arr: number | boolean | RegularArray<number> | RegularArray<boolean>,
+    ret: Array<number | boolean> = []): Array<number | boolean> {
+  if (Array.isArray(arr)) {
+    for (let i = 0; i < arr.length; ++i) {
+      flatten(arr[i], ret);
+    }
+  } else {
+    ret.push(arr);
+  }
+  return ret;
+}
+
+export function inferShape(arr: number | boolean | RegularArray<number> |
+                           RegularArray<boolean>): number[] {
+  const shape: number[] = [];
+  while (arr instanceof Array) {
+    shape.push(arr.length);
+    arr = arr[0];
+  }
+  return shape;
 }
