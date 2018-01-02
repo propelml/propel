ca642179d35a9f1fd043ff2a4fb499e3d1c6019a
Ryan Dahl <ry@tinyclouds.org>
2018-01-02 01:16:17 -0500

    Improve tensor formating.

--- format_test.ts
+++ format_test.ts
@@ -1,19 +1,42 @@
-import * as format from "./format";
+import { toString } from "./format";
 import { assert } from "./util";
 
 function test1DInt32() {
-  const actual = format.toString([4], new Int32Array([1, 2, 3, 4]));
-  const expected = "[ 1, 2, 3, 4 ]";
-  assert(actual === expected);
+  const actual = toString([4], new Int32Array([1, 2, 3, 4]));
+  const expected = "[1, 2, 3, 4]";
   console.log(actual);
+  assert(actual === expected);
 }
 
 function test2DInt32() {
-  const actual = format.toString([2, 2], new Int32Array([1, 2, 3, 4]));
-  const expected = "[ [ 1, 2 ],\n  [ 3, 4 ] ]";
-  assert(actual === expected);
+  const actual = toString([2, 2], new Int32Array([1, 2, 3, 4]));
+  const expected = "[[1, 2],\n [3, 4]]";
   console.log(actual);
+  assert(actual === expected);
+}
+
+function test1DFloat32() {
+  const d = [ 1.2,  0. ,  0. ,  0.123];
+  const actual = toString([4], new Float32Array(d));
+  const expected = "[ 1.2  ,  0.   ,  0.   ,  0.123]";
+  console.log(actual);
+  assert(actual === expected);
+}
+
+function test2DFloat32() {
+  let actual = toString([2, 2], new Float32Array([1, 2, 3, 4]));
+  let expected = "[[ 1.,  2.],\n [ 3.,  4.]]";
+  console.log(actual);
+  assert(actual === expected);
+
+  const ta = new Float32Array([1.2, 0, 0, 0.1, 1.2, 0, 0.5, 0]);
+  actual = toString([2, 4], ta);
+  expected = "[[ 1.2,  0. ,  0. ,  0.1],\n [ 1.2,  0. ,  0.5,  0. ]]";
+  console.log(actual);
+  assert(actual === expected);
 }
 
 test1DInt32();
 test2DInt32();
+test1DFloat32();
+test2DFloat32();
