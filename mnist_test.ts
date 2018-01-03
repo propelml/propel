d97e0c7055ddb2f98d150c9a20c04abf050c81dd
Bert Belder <bertbelder@gmail.com>
2018-01-03 20:34:09 +0100

    test: use async functions in mnist test

--- mnist_test.ts
+++ mnist_test.ts
@@ -2,30 +2,28 @@ import * as mnist from "./mnist";
 import { test } from "./test";
 import { assert, assertAllEqual, assertShapesEqual } from "./util";
 
-test(function testTrainSplit() {
+test(async function testMnistTrainSplit() {
   const dataset = mnist.load("train", 256, false);
-  return dataset.next().then(({images, labels}) => {
-    assertShapesEqual(images.shape, [256, 28, 28]);
-    console.log("images dtype", images.dtype);
-    // assert(images.dtype === "uint8");
-    assertShapesEqual(labels.shape, [256]);
-    // assert(labels.dtype === "uint8");
-    // > hexdump deps/mnist/train-labels-idx1-ubyte | head -1
-    // 0000000 00 00 08 01 00 00 ea 60 05 00 04 01 09 02 01 03
-    //         ---magic--- ---items--- ---------labels--------
-    assertAllEqual(labels.slice([0], [8]), [5, 0, 4, 1, 9, 2, 1, 3]);
-    console.log("TrainSplit ok");
-  });
+  const {images, labels} = await dataset.next();
+  assertShapesEqual(images.shape, [256, 28, 28]);
+  console.log("images dtype", images.dtype);
+  // assert(images.dtype === "uint8");
+  assertShapesEqual(labels.shape, [256]);
+  // assert(labels.dtype === "uint8");
+  // > hexdump deps/mnist/train-labels-idx1-ubyte | head -1
+  // 0000000 00 00 08 01 00 00 ea 60 05 00 04 01 09 02 01 03
+  //         ---magic--- ---items--- ---------labels--------
+  assertAllEqual(labels.slice([0], [8]), [5, 0, 4, 1, 9, 2, 1, 3]);
+  console.log("TrainSplit ok");
 });
 
-test(function testTestSplit() {
+test(async function testMnistTestSplit() {
   const dataset = mnist.load("test", 16, false);
-  return dataset.next().then(({images, labels}) => {
-    assertShapesEqual(images.shape, [16, 28, 28]);
-    // assert(images.dtype === "uint8");
-    assertShapesEqual(labels.shape, [16]);
-    // assert(labels.dtype === "uint8");
-    assertAllEqual(labels.slice([0], [3]), [7, 2, 1]);
-    console.log("TestSplit ok");
-  });
+  const {images, labels} = await dataset.next();
+  assertShapesEqual(images.shape, [16, 28, 28]);
+  // assert(images.dtype === "uint8");
+  assertShapesEqual(labels.shape, [16]);
+  // assert(labels.dtype === "uint8");
+  assertAllEqual(labels.slice([0], [3]), [7, 2, 1]);
+  console.log("TestSplit ok");
 });
