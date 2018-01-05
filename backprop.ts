84c17e2aa9b61ce4bbcf42ffd3da67dcf34825cf
Ryan Dahl <ry@tinyclouds.org>
2018-01-05 16:52:02 -0500

    Update copyright.

--- backprop.ts
+++ backprop.ts
@@ -1,3 +1,17 @@
+/*
+   Copyright 2018 Propel http://propel.site/.  All rights reserved.
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+ */
 // This implementation closely follows gradients_function from TF Eager:
 // tslint:disable-next-line:max-line-length
 // https://github.com/tensorflow/tensorflow/blob/16b0bb095296fcfa17182aeae656a35faf70f36e/tensorflow/python/eager/backprop.py#L442
