d646e80a9ae0c9dd699161b7153191d0353c0647
Ryan Dahl <ry@tinyclouds.org>
2018-01-05 16:41:03 -0500

    Lint check.h

--- binding.cc
+++ binding.cc
@@ -1,5 +1,5 @@
 /*
-   Copyright 2017 propel authors. All Rights Reserved.
+   Copyright 2017 Propel authors. All Rights Reserved.
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
    You may obtain a copy of the License at
