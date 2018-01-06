591cb3e73c4dbf2fc996df7f6af39c3b04e9bf9f
yungbeto <roby.saavedra@gmail.com>
2018-01-06 18:09:02 -0500

    Design work.

--- tools/stylelint.js
+++ tools/stylelint.js
@@ -2,7 +2,9 @@
 // To just run the linter: ./tools/stylelint.js
 // To auto-format code:    ./tools/stylelint.js --fix
 const run = require('./run');
+const extra = process.argv.slice(2).join(" ");
 run.sh(`node ./node_modules/stylelint/bin/stylelint.js
+  ${extra}
   --config stylelint.json
   website/style.css
   website/syntax.css`);
