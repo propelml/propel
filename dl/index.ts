fbfba0b07a66d76170877a9392d017b7ad0c3f09
Bert Belder <bertbelder@gmail.com>
2018-01-08 22:54:52 +0100

    dl: remove stuff

--- dl/index.ts
+++ dl/index.ts
@@ -15,7 +15,6 @@
  * =============================================================================
  */
 
-import * as xhr_dataset from './data/xhr-dataset';
 import * as environment from './environment';
 import * as gpgpu_util from './math/backends/webgl/gpgpu_util';
 // tslint:disable-next-line:max-line-length
@@ -26,24 +25,7 @@ import * as test_util from './test_util';
 import * as util from './util';
 import {version} from './version';
 
-export {CheckpointLoader} from './data/checkpoint_loader';
-export {DataStats, InMemoryDataset} from './data/dataset';
-// tslint:disable-next-line:max-line-length
-export {InCPUMemoryShuffledInputProviderBuilder, InGPUMemoryShuffledInputProviderBuilder, InputProvider} from './data/input_provider';
-export {XhrDataset, XhrDatasetConfig, XhrModelConfig} from './data/xhr-dataset';
 export {ENV, Environment, Features} from './environment';
-export {Graph, Tensor} from './graph/graph';
-export {AdadeltaOptimizer} from './graph/optimizers/adadelta_optimizer';
-export {AdagradOptimizer} from './graph/optimizers/adagrad_optimizer';
-export {AdamOptimizer} from './graph/optimizers/adam_optimizer';
-export {AdamaxOptimizer} from './graph/optimizers/adamax_optimizer';
-export {MomentumOptimizer} from './graph/optimizers/momentum_optimizer';
-export {Optimizer} from './graph/optimizers/optimizer';
-export {RMSPropOptimizer} from './graph/optimizers/rmsprop_optimizer';
-export {SGDOptimizer} from './graph/optimizers/sgd_optimizer';
-export {CostReduction, FeedEntry, Session} from './graph/session';
-// tslint:disable-next-line:max-line-length
-export {GraphRunner, GraphRunnerEventObserver, MetricReduction} from './graph_runner';
 // tslint:disable-next-line:max-line-length
 export {ConstantInitializer, Initializer, NDArrayInitializer, OnesInitializer, RandomNormalInitializer, RandomTruncatedNormalInitializer, RandomUniformInitializer, VarianceScalingInitializer, ZerosInitializer} from './initializers';
 export {MathBackendCPU, NDArrayMathCPU} from './math/backends/backend_cpu';
@@ -54,7 +36,6 @@ export {GPGPUContext} from './math/backends/webgl/gpgpu_context';
 export {LSTMCell, NDArrayMath} from './math/math';
 // tslint:disable-next-line:max-line-length
 export {Array1D, Array2D, Array3D, Array4D, NDArray, Scalar} from './math/ndarray';
-export {Model} from './model';
 export {version};
 // Second level exports.
 export {
@@ -64,6 +45,5 @@ export {
   render_ndarray_gpu_util,
   test_util,
   util,
-  webgl_util,
-  xhr_dataset
+  webgl_util
 };
