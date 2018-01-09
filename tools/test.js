56c73e31308a57aaba439899738f0724449a7c26
Ryan Dahl <ry@tinyclouds.org>
2018-01-09 00:57:27 -0500

    Introduce npm packages for binding like propel_mac.
    They are called:
    propel_mac
    propel_windows
    propel_linux
    propel_linux_gpu
    
    Currently only tested on propel_mac.

--- tools/test.js
+++ tools/test.js
@@ -2,12 +2,12 @@
 const run = require("./run");
 
 // Build the project.
-run.sh("node ./tools/build.js");
-run.sh("node ./node_modules/typescript/bin/tsc");
+run.sh("node ./tools/build_binding.js");
+run.sh("node ./tools/tsc.js");
 run.sh("node ./tools/webpack.js");
 // Node.js tests
-run.sh("node dist/test_node.js", {"PROPEL": "dl"});     // DL backend
-run.sh("node dist/test_node.js", {"PROPEL": "tf"});     // TF backend
-run.sh("node dist/binding_test.js", {"PROPEL": "tf"});  // TF-only test. 
+run.sh("node build/test_node.js", {"PROPEL": "dl"});     // DL backend
+run.sh("node build/test_node.js", {"PROPEL": "tf"});     // TF backend
+run.sh("node build/binding_test.js", {"PROPEL": "tf"});  // TF-only test. 
 // Web browser tests
-run.sh("node dist/test_browser.js");
+run.sh("node build/test_browser.js");
