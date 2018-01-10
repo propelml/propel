d8a397712a8b2a7aa205ac5a50d65c332959108f
Bert Belder <bertbelder@gmail.com>
2018-01-10 02:09:09 +0100

    test: fix browser test failures not being detected

--- test_browser.ts
+++ test_browser.ts
@@ -21,7 +21,11 @@ const TESTS = [
   // The test harness logs "DONE bla bla" to the console when done.
   // If this message doesn't appear, or an unhandled error is thrown on the
   // page, the test fails.
-  { href: "test_isomorphic.html", doneMsg: /^DONE/, timeout: 20000 },
+  {
+    doneMsg: /^DONE.*failed: 0/,
+    href: "test_isomorphic.html",
+    timeout: 30 * 1000
+  },
 
   // These web pages are simply loaded; the test passes if no unhandled
   // exceptions are thrown on the page.
