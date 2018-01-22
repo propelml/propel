34a6bf4113aebe2433d4a684cb3f56cb51f2c2d0
Ryan Dahl <ry@tinyclouds.org>
2018-01-22 13:16:10 -0500

    Fix stylelint references.

--- tools/stylelint.js
+++ tools/stylelint.js
@@ -3,8 +3,9 @@
 // To auto-format code:    ./tools/stylelint.js --fix
 const run = require('./run');
 const extra = process.argv.slice(2).join(" ");
+// TODO use *.scss here.
 run.sh(`node ./node_modules/stylelint/bin/stylelint.js
   ${extra}
   --config stylelint.json
-  website/style.css
-  website/syntax.css`);
+  website/main.scss
+  website/syntax.scss`);
