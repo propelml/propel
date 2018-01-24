891741a37d01647e26d552b7d9a92f6686cfe824
Bert Belder <bertbelder@gmail.com>
2018-01-23 16:48:43 -0800

    ci: also suppress webpack progress on Travis CI

--- tools/webpack.js
+++ tools/webpack.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 const run = require('./run');
 let extra = process.argv.slice(2).join(" ");
-if (!process.env.APPVEYOR) extra += " --progress";
+if (!process.env.CI) extra += " --progress";
 run.sh(`node ./node_modules/webpack/bin/webpack.js
   --config webpack.config.js
   ${extra}
