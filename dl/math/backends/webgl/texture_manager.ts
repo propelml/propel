c3d1b29b5869edfbd6e43db9bc3e04ab410ed94f
Bert Belder <bertbelder@gmail.com>
2018-01-25 13:59:40 -0800

    dl: reduce to skeletton textureManager

--- dl/math/backends/webgl/texture_manager.ts
+++ dl/math/backends/webgl/texture_manager.ts
@@ -19,80 +19,24 @@ import {GPGPUContext} from './gpgpu_context';
 
 export class TextureManager {
   private numUsedTextures = 0;
-  private numFreeTextures = 0;
-  private freeTextures: {[shape: string]: WebGLTexture[]} = {};
-  private logEnabled = false;
-  private allocatedTextures: WebGLTexture[] = [];
-  private usedTextureCount: {[shape: string]: number} = {};
 
   constructor(private gpgpu: GPGPUContext) {}
 
   acquireTexture(shapeRC: [number, number]): WebGLTexture {
-    const shapeKey = getKeyFromTextureShape(shapeRC);
-    if (!(shapeKey in this.freeTextures)) {
-      this.freeTextures[shapeKey] = [];
-    }
-    if (!(shapeKey in this.usedTextureCount)) {
-      this.usedTextureCount[shapeKey] = 0;
-    }
-    this.usedTextureCount[shapeKey]++;
-
-    if (this.freeTextures[shapeKey].length > 0) {
-      this.numFreeTextures--;
-      this.numUsedTextures++;
-      this.log();
-      return this.freeTextures[shapeKey].shift();
-    }
     this.numUsedTextures++;
-    this.log();
-
     const newTexture = this.gpgpu.createMatrixTexture(shapeRC[0], shapeRC[1]);
-    this.allocatedTextures.push(newTexture);
     return newTexture;
   }
 
   releaseTexture(texture: WebGLTexture, shape: [number, number]): void {
-    const shapeKey = getKeyFromTextureShape(shape);
-    if (!(shapeKey in this.freeTextures)) {
-      this.freeTextures[shapeKey] = [];
-    }
-    this.freeTextures[shapeKey].push(texture);
-    this.numFreeTextures++;
     this.numUsedTextures--;
-    this.usedTextureCount[shapeKey]--;
-    this.log();
-  }
-
-  private log() {
-    if (!this.logEnabled) {
-      return;
-    }
-    const total = this.numFreeTextures + this.numUsedTextures;
-    console.log(
-        'Free/Used', `${this.numFreeTextures} / ${this.numUsedTextures}`,
-        `(${total})`);
+    this.gpgpu.deleteMatrixTexture(texture);
   }
 
   getNumUsedTextures(): number {
     return this.numUsedTextures;
   }
 
-  getNumFreeTextures(): number {
-    return this.numFreeTextures;
-  }
-
   dispose() {
-    this.allocatedTextures.forEach(texture => {
-      this.gpgpu.deleteMatrixTexture(texture);
-    });
-    this.freeTextures = null;
-    this.allocatedTextures = null;
-    this.usedTextureCount = null;
-    this.numUsedTextures = 0;
-    this.numFreeTextures = 0;
   }
-}
-
-function getKeyFromTextureShape(shapeRowsCol: [number, number]): string {
-  return `${shapeRowsCol[0]}_${shapeRowsCol[1]}`;
 }
