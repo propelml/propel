219f4469c12e2de4e558e816105d4a1cedcc285c
Bert Belder <bertbelder@gmail.com>
2018-02-01 21:32:09 -0800

    dl: remove kernel registry, executeKernel, and related types

--- dl/math/backends/types/matmul.ts
+++ dl/math/backends/types/matmul.ts
@@ -15,26 +15,6 @@
  * =============================================================================
  */
 
-import {NamedArrayMap} from '../../../util';
-import {Array2D} from '../../ndarray';
-// tslint:disable-next-line:max-line-length
-import {KernelInputConfig, KernelNode} from '../tape_types';
-
-export interface MatMulNode extends KernelNode {
-  inputAndArgs: MatMulInputConfig;
-  output: Array2D;
-}
-
-export interface MatMulInputConfig extends KernelInputConfig {
-  inputs: MatMulInputArrays;
-  args: {aOrientation: MatrixOrientation; bOrientation: MatrixOrientation};
-}
-
-export interface MatMulInputArrays extends NamedArrayMap {
-  a: Array2D;
-  b: Array2D;
-}
-
 export enum MatrixOrientation {
   REGULAR,
   TRANSPOSED
