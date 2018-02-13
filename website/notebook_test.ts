e7bd45e9e7e64a3361e0054b42cd7374f4a86eb1
Ryan Dahl <ry@tinyclouds.org>
2018-02-13 14:23:05 -0500

    Fixes Shift+Enter behavior.
    Original version of this patch by Parsa Ghadimi <qti3eqti3e@gmail.com>
    (#248)

--- website/notebook_test.ts
+++ website/notebook_test.ts
@@ -1,5 +1,5 @@
 import { h, render } from "preact";
-import { assert, objectsEqual } from "../src/util";
+import { assert, delay, objectsEqual } from "../src/util";
 import { testBrowser } from "../tools/tester";
 import { enableMock } from "./db";
 import * as nb from "./notebook";
@@ -41,4 +41,44 @@ testBrowser(async function notebook_Notebook() {
   // Because we aren't logged in, we shouldn't see any clone button.
   const clones = document.getElementsByClassName("clone");
   assert(0 === clones.length);
+});
+
+testBrowser(async function notebook_focusNextCell() {
+  const mdb = enableMock();
+  document.body.innerHTML = "";
+
+  const readyPromise = new Promise((resolve) => {
+    const el = h(nb.Notebook, {
+      nbId: "default",
+      onReady: resolve,
+    });
+    render(el, document.body);
+  });
+
+  await readyPromise;
+
+  assert(objectsEqual(mdb.counts, { getDoc: 1 }));
+  // Test focusNextCell transitions.
+  const cellEls = document.getElementsByClassName("notebook-cell");
+  assert(cellEls.length >= 2);
+  const first = nb.lookupCell(cellEls[0].id);
+  const second = nb.lookupCell(cellEls[1].id);
+
+  first.focus();
+
+  assert(cellEls[0].classList.contains("notebook-cell-focus"));
+  assert(!cellEls[1].classList.contains("notebook-cell-focus"));
+
+  // Simulate Shift+Enter
+  first.editor.triggerOnKeyDown({
+    keyCode: 13,  // Enter
+    shiftKey: true,
+  });
+
+  // TODO This is a race condition. Replace this with something like
+  // await rerender();
+  await delay(100);
+
+  assert(!cellEls[0].classList.contains("notebook-cell-focus"));
+  assert(cellEls[1].classList.contains("notebook-cell-focus"));
 });
