709ad2b0067fb59eae70484fa6efa8294d868e73
Bert Belder <bertbelder@gmail.com>
2018-02-20 17:40:30 +0800

    website: support setting an overlay image to help with development

--- website/website_main.ts
+++ website/website_main.ts
@@ -11,4 +11,26 @@ enableFirebase();
 window.addEventListener("load", () => {
   render(h(Router, null), document.body, document.body.children[0]);
   drainExecuteQueue();
+
+  const match = /(?:^#|&)overlay=(.*?)(?:&|$)/.exec(window.location.hash);
+  if (match) {
+    const image = match[1];
+    const origStyle = Object.assign({}, document.body.style);
+    document.addEventListener("keydown", e => {
+      if (e.key !== "Control") return;
+      Object.assign(document.body.style, {
+        opacity: .5,
+        backgroundImage: `url("/static/${image}")`,
+        backgroundRepeat: "no-repeat",
+        backgroundPositionX: "center",
+        backgroundPositionY: "top",
+      });
+      e.stopPropagation();
+    });
+    document.addEventListener("keyup", e => {
+      if (e.key !== "Control") return;
+      Object.assign(document.body.style, origStyle);
+      e.stopPropagation();
+    });
+  }
 });
