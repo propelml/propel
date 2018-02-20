1b2e06b474dee8423b1b66612c366c8d4dac527b
Bert Belder <bertbelder@gmail.com>
2018-02-20 19:10:42 +0800

    gendoc: fix lint issues

--- tools/gendoc_test.ts
+++ tools/gendoc_test.ts
@@ -12,9 +12,9 @@
    See the License for the specific language governing permissions and
    limitations under the License.
  */
-import * as gendoc from "./gendoc";
 import { test } from "../test";
 import { assert } from "../util";
+import * as gendoc from "./gendoc";
 
 test(async function gendoc_smoke() {
   const docs = gendoc.genJSON();
@@ -26,4 +26,3 @@ test(async function gendoc_smoke() {
   assert(names.indexOf("Tensor.add") >= 0);
   assert(names.indexOf("Tensor[Symbol.iterator]") >= 0);
 });
-
