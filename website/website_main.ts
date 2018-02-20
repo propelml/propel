462509b8d58c6c6bfd8bea0f8edab4f3b7140959
Ryan Dahl <ry@tinyclouds.org>
2018-02-20 18:27:50 -0500

    Fix "Navigation Timeout Exceeded: 30000ms exceeded"
    - Add output to onload callback in website_main to ouput
      a special string "Propel onload".
    - Check for that string in the tests.
    - Disable puppeteer's timeout.
    - rerender() used in nodebook_test instead of delay.
    - Various code clean ups.
    - Only rerender in website_main during tests.

--- website/website_main.ts
+++ website/website_main.ts
@@ -1,5 +1,5 @@
-import { h, render } from "preact";
-import { assert, IS_WEB } from "../src/util";
+import { h, render, rerender } from "preact";
+import { assert, delay, IS_WEB } from "../src/util";
 import { enableFirebase } from "./db";
 import { drainExecuteQueue } from "./notebook";
 import { Router } from "./website";
@@ -8,7 +8,14 @@ assert(IS_WEB);
 
 enableFirebase();
 
-window.addEventListener("load", () => {
+window.addEventListener("load", async() => {
   render(h(Router, null), document.body, document.body.children[0]);
-  drainExecuteQueue();
+  await drainExecuteQueue();
+  // If we're in a testing environment...
+  if (window.navigator.webdriver) {
+    // rerender to make sure the dom is up to date and then output a special
+    // string which is used in tools/test_browser.js.
+    rerender();
+    console.log("Propel onload");
+  }
 });
