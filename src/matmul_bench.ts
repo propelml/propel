89a16402b3401ee3e53635a745a600f9fb2ac751
Bert Belder <bertbelder@gmail.com>
2018-02-21 20:25:24 +0800

    dl: optimize cpu matmul

---
+++ src/matmul_bench.ts
@@ -0,0 +1,28 @@
+
+import { ones } from "./api";
+
+const t1 = ones([100, 100]);
+const t2 = ones([100, 100]).add(1);
+
+let count = 100;
+let total = 0;
+const begin = Date.now() / 1000;
+let end = begin;
+
+for (let i = 0; i < 20; i++) {
+  const start = end;
+
+  for (let i = 0; i < count; i++) {
+    const t3 = t1.matmul(t2);
+  }
+
+  end = Date.now() / 1000;
+  const elapsed = end - start;
+  const throughput = count / elapsed;
+
+  total += count;
+  count = Math.round(total / (end - begin));
+
+  console.log(
+    `time: ${elapsed}s  count: ${count}  throughput: ${throughput}/s`);
+}
