206869024117c19fdad3246056e86db51de66c3e
Ryan Dahl <ry@tinyclouds.org>
2018-02-23 05:38:46 -0500

    Remove old nn_example.
    Also:
    - Removes unused mnist code.
    - Removes gradParams2 and top-level tape.

--- src/mnist_test.ts
+++ src/mnist_test.ts
@@ -14,36 +14,23 @@
  */
 import { test } from "../tools/tester";
 import * as mnist from "./mnist";
-import { assert, assertAllEqual, assertShapesEqual } from "./util";
+import { assertAllEqual, assertShapesEqual } from "./util";
 
 test(async function mnist_trainSplit() {
-  const dataset = mnist.load("train", 256, false);
-  const {images, labels} = await dataset.next();
-
-  assertShapesEqual(images.shape, [256, 28, 28]);
-  // TODO Ultimately the mnist dataset should return uint8 tensors for the
-  // images. However due to a performance workaround, we are using float32
-  // for now.
-  assert(images.dtype === "float32");
+  const { images, labels } = await mnist.loadSplit("train");
+  assertShapesEqual(images.shape, [60000, 28, 28]);
   assertAllEqual(images.reduceMax(), 255);
-  assertAllEqual(images.reduceMean(), 32.7086296081543);
-
-  assertShapesEqual(labels.shape, [256]);
+  assertShapesEqual(labels.shape, [60000]);
   // assert(labels.dtype === "uint8");
   // > hexdump deps/mnist/train-labels-idx1-ubyte | head -1
   // 0000000 00 00 08 01 00 00 ea 60 05 00 04 01 09 02 01 03
   //         ---magic--- ---items--- ---------labels--------
   assertAllEqual(labels.slice([0], [8]), [5, 0, 4, 1, 9, 2, 1, 3]);
-  console.log("TrainSplit ok");
 });
 
 test(async function mnist_testSplit() {
-  const dataset = mnist.load("test", 16, false);
-  const {images, labels} = await dataset.next();
-  assertShapesEqual(images.shape, [16, 28, 28]);
-  // assert(images.dtype === "uint8");
-  assertShapesEqual(labels.shape, [16]);
-  // assert(labels.dtype === "uint8");
+  const {images, labels} = await mnist.loadSplit("test");
+  assertShapesEqual(images.shape, [10000, 28, 28]);
+  assertShapesEqual(labels.shape, [10000]);
   assertAllEqual(labels.slice([0], [3]), [7, 2, 1]);
-  console.log("TestSplit ok");
 });
