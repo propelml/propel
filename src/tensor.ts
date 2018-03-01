510d9d993103091f668d4c711ee26b5651d93d48
Ryan Lewis <ryanharrisonlewis@gmail.com>
2018-03-01 12:01:46 -0700

    docs

--- src/tensor.ts
+++ src/tensor.ts
@@ -185,10 +185,24 @@ export class Tensor implements types.BasicTensor {
     return ops.cast(this, dtype);
   }
 
+  /** Adds two tensors and returns the resulting tensor.
+   *
+   *    import { tensor } from "propel";
+   *    const x = tensor(1);
+   *    const y = tensor(2);
+
+   *    const result = x.add(y);
+   */
   add(x: types.TensorLike): Tensor {
     return ops.add(this, this.colocate(x));
   }
 
+  /** In-place version of add().
+   *
+   *    import { tensor } from "propel";
+   *    const x = tensor(1);
+   *    x.add_(1);
+   */
   add_(x: types.TensorLike): Tensor {
     const result = ops.add(this, this.colocate(x));
 
