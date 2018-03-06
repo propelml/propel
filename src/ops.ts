06774fdb2332b6d47b55a0017a16ccde0ef07d10
Ryan Dahl <ry@tinyclouds.org>
2018-03-06 16:57:24 -0500

    Add conv2d op.

--- src/ops.ts
+++ src/ops.ts
@@ -511,3 +511,19 @@ defBW("setDiag", (g) => {
   // return bo.getDiag(g);
   throw new Error("Not Implemented.");
 });
+
+export const conv2d = defFW("conv2d",
+  (input: types.BasicTensor, filter: types.BasicTensor,
+   opts: types.ConvOpts) => {
+    saveForBackward(input, filter, opts);
+    return bo.conv2d(input, filter, opts);
+  });
+defBW("conv2d",
+  (g, input, filter, opts) => {
+    const b = bo.conv2dBackpropFilter(g, input, filter.shape, opts);
+    return convert(b);
+  },
+  (g, input, filter, opts) => {
+    const b =  bo.conv2dBackpropInput(g, input.shape, filter, opts);
+    return convert(b);
+  });
