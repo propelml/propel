73737e11789e90aaffa6876e3d5d674e2d235f9b
Ryan Dahl <ry@tinyclouds.org>
2018-03-10 21:06:53 -0500

    deps: add ncp, remove jsdom

--- tools/build_website.js
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env node
-// This file is just to easily run typescript from the command-line without
-// requiring that ts-node is installed. All of the logic for building the
-// website is in tools/build_website_impl.ts
-require("ts-node").register({"typeCheck": true });
-require("./build_website_impl.ts");
