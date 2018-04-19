06a58afb6582cdcbd2c3bc0a6529bf5d4ca17bf7
Ryan Dahl <ry@tinyclouds.org>
2018-04-19 12:24:50 -0400

    TFJS message

--- README.md
+++ README.md
@@ -1,129 +1,14 @@
-# Propel
+April 19, 2018
 
-Differential Programming in JavaScript.
+TensorFlow.js was recently released. It is well engineered, provides an
+autograd-style interface to backprop, and has committed to supporting Node.
+This satisfies our requirements. It is counterproductive to pursue a parallel
+effort. Thus we are abandoning our backprop implementation, TF C binding, and
+the TF/DL bridge, which made up the foundation of the Propel library. We intend
+to rebase our work on top of TFJS.
 
-| **Linux & Mac** | **Windows** |
-|:---------------:|:-----------:|
-| [![][Travis CI badge]][Travis CI link] | [![][AppVeyor badge]][AppVeyor link] |
+Our high-level goal continues to be a productive workflow for scientific
+computing in JavaScript. Building on top of TFJS allows us to focus on
+higher-level functionality.
 
-## Status and Examples
-
-Propel is under heavy development and its API, sadly, is subject to change.
-We do intend to make a Promise of Compatibility in the future; but
-it can't be done yet.
-
-Please have a look at our example neural network here
-https://github.com/propelml/propel/blob/master/example.js
-
-Also the documentation has many examples:
-http://propelml.org/docs
-
-We have a real-time chat at https://gitter.im/propelml/propel
-And a twitter https://twitter.com/propelml
-
-
-## How to run TF examples
-
-    ./tools/build.js
-    node example.js
-
-If you're on Linux and would like to build a CUDA version of
-Propel set the environmental variable `PROPEL_BUILD_GPU=1`.
-
-    PROPEL_BUILD_GPU=1 ./tools/build.js
-
-
-## Packages
-
-npm packages are built with `./tools/package.js`. Here are links
-to the various packages:
-
-    https://www.npmjs.com/package/propel
-    https://www.npmjs.com/package/propel_linux
-    https://www.npmjs.com/package/propel_linux_gpu
-    https://www.npmjs.com/package/propel_mac
-    https://www.npmjs.com/package/propel_windows
-
-
-## Contributing
-
-Check out Propel, including its git submodules.
-
-    git clone --recurse-submodules https://github.com/propelml/propel.git
-
-Propel generally bundles its dependencies in the "deps" submodule.
-
-To run the complete test suite:
-
-    ./tools/presubmit.js
-
-One unincluded dependency is a chromium binary for running tests.
-If puppeteer complains that chromium hasn't been downloaded, run:
-
-    npm rebuild puppeteer
-
-Most of the tests are run on every pull request, however there are three sets
-of tests which currently must be run manually:
-
- 1. Check that tests pass on a Linux machine with CUDA.
-    Run this command: `PROPEL_BUILD_GPU=1 ./tools/presubmit.js`
-
- 2. Check that the WebGL backend works. (Puppeteer headless doesn't support
-    WebGL.)
-    Run this command: `PP_TEST_DEBUG=1 ts-node tools/test_browser.ts`
-
- 3. Check that the DeepLearn tests run. (These tests also require WebGL)
-    Run this command:  `PP_TEST_DL=1 ts-node tools/test_browser.ts`
-
-
-### Working on the website
-
-Propel has two major components:
-
- 1. the core framework, located in `src/`, and
-
- 2. the website, located in `website/`.
-
-Due to the notebook and documentation, the website is fairly non-trivial. It
-is written in React (actually Preact).
-
-When working on the website, it's often nice to run a dev server
-
-    ./tools/dev_website.js
-
-This will automatically rebuild the website on any scss or javascript
-changes.
-
-To run the website tests, point your browser to
-http://localhost:8080/static/test.html#script=/test_website.js
-
-To build the production website, `./tools/website_render.js prod` must be run.
-
-
-### Adding An Op
-
-Propel is under heavy development and is missing implementations for many common
-ops. Here is a rough outline of how to add an op
-
-  1. Add the frontend implementation to `api.ts` or `tensor.ts` (depending on
-     if its a method of Tensor or a standalone function)
-
-  2. Add the op's signature to the `BackendOps` interface in `types.ts`.
-
-  3. Add the forward and backwards passes to `ops.ts`.
-
-  4. Finally implement the op for DL and TF in `dl.ts` and `tf.ts`
-     respectively.
-
-  5. Add a test demonstrating the desired behavior in `src/api_test.ts`.
-     The tensorflow binding can be built using `./tools/build_tf_binding.js`
-     and the test can be run by doing `ts-node src/api_test.ts MyTest`.
-     The DL test can be run by setting an environmental variable:
-     `PROPEL=dl ts-node src/api_test.ts MyTest`
-
-
-
-[AppVeyor badge]:  https://ci.appveyor.com/api/projects/status/github/propelml/propel?branch=master&svg=true
-[AppVeyor link]:   https://ci.appveyor.com/project/piscisaureus/propel/branch/master
-[Travis CI badge]: https://travis-ci.org/propelml/propel.svg?branch=master
-[Travis CI link]:  https://travis-ci.org/propelml/propel/builds
+We have no release at this time.
--- website/index.tsx
+++ website/index.tsx
@@ -13,189 +13,24 @@
    limitations under the License.
  */
 // tslint:disable:variable-name
+import { readFileSync } from "fs";
 import { h } from "preact";
-import { Footer, GlobalHeader, UserMenu } from "./common";
-import * as nb from "./nb";
-const { version } = require("../package.json");
+import { GlobalHeader } from "./common";
 
 export const PropelIndex = (props) => {
+  let md = readFileSync(__dirname + "/../README.md", "utf8");
+  md = "<p>" + md.replace(/\n\n/g, "\n\n<p>");
   return (
     <div class="index">
-      <GlobalHeader>
-        <a href="/notebook">Notebook</a>
-        <UserMenu { ...props } />
-      </GlobalHeader>
-      <Intro />
-      <UseIt />
-      <Perks />
-      <TrainingExample />
-      <Footer />
+      <GlobalHeader />
+      <div class="intro flex-row">
+        <div class="flex-cell">
+          <div dangerouslySetInnerHTML={ { __html: md } } />
+          <p><a href="http://github.com/propelml/propel"
+            >github.com/propelml/propel</a>
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
-
-const Intro = () => (
-  <div class="intro flex-row">
-    <div class="flex-cell">
-      <h2 class="intro__header">Differential Programming in JavaScript</h2>
-      <p>
-        <b>Propel</b> provides a GPU-backed numpy-like infrastructure
-        for scientific computing in JavaScript.  JavaScript is a fast,
-        dynamic language which, we think, could act as an ideal workflow
-        for scientific programmers of all sorts.
-      </p>
-      { headerButton("/docs", "API Ref") }
-      { headerButton("http://github.com/propelml/propel", "Github") }
-    </div>
-    <div class="intro__notebook flex-cell">
-      { nb.cell(tanhGrads) }
-    </div>
-  </div>
-);
-
-const tanhGrads = `
-import { grad, linspace, plot } from "propel";
-
-f = x => x.tanh();
-x = linspace(-4, 4, 200);
-plot(x, f(x),
-     x, grad(f)(x),
-     x, grad(grad(f))(x),
-     x, grad(grad(grad(f)))(x),
-     x, grad(grad(grad(grad(f))))(x))
-`;
-
-const UseIt = () => (
-  <div class="use-it">
-    <div class="use-it__inner">
-      <p class="use-it__snippit-title">Use it in Node:</p>
-      { fixed("npm install propel\nlet pr = require(\"propel\");") }
-      <p class="use-it__snippit-title">Use it in a browser:</p>
-      { fixed(`<script src="https://unpkg.com/propel@${version}"></script>`) }
-    </div>
-  </div>
-);
-
-const Perks = () => (
-  <div class="perks">
-    <div class="flex-row">
-      <div class="flex-cell">
-        <h2 class="perks__header perks--world">Run anywhere.</h2>
-        <p>
-          Propel runs both in the browser and natively from Node. In
-          both environments Propel is able to use GPU hardware for
-          computations.  In the browser it utilizes WebGL through
-          &nbsp;
-          <a href="https://deeplearnjs.org/">deeplearn.js</a>
-          &nbsp;
-          and on Node it uses TensorFlow's
-          &nbsp;
-          <a href="https://www.tensorflow.org/install/install_c">C API</a>.
-        </p>
-      </div>
-      <div class="flex-cell">
-        <h2 class="perks__header perks--upward">Phd optional.</h2>
-        <p>
-          Propel has an imperative
-          &nbsp;
-          <a href="https://github.com/HIPS/autograd">autograd</a>
-          -style API.  Computation graphs are traced as
-          you run them -- a general purpose
-          &nbsp;
-          <i>gradient function</i>
-          &nbsp;
-          provides an elegant interface to backpropagation.
-        </p>
-      </div>
-    </div>
-    <div class="flex-row">
-      <div class="flex-cell">
-        <h2 class="perks__header perks--chip">Did somebody say GPUs?</h2>
-        <p>
-          Browsers are great for demos, but they are not a great numerics
-          platform. WebGL is a far cry from CUDA. By running Propel outside
-          of the browser, users will be able to target multiple GPUs and
-          make TCP connections. The models developed server-side will be
-          much easier to deploy as HTML demos.
-        </p>
-      </div>
-      <div class="flex-cell">
-        <h2 class="perks__header perks--lightning">Let's do this.</h2>
-        <p>
-          The basic propel npm package is javascript only,
-          without TensorFlow bindings. To upgrade your speed dramatically
-          install
-        </p>
-        { fixed([
-            "npm install propel_mac",
-            "npm install propel_windows",
-            "npm install propel_linux",
-            "npm install propel_linux_gpu",
-          ].join("\n")) }
-      </div>
-    </div>
-  </div>
-);
-
-// TODO Use require instead of import in the trainingExampleCode.
-// So people can copy and past the code into the terminal.
-const trainingExampleCode = `
-import * as pr from "propel"
-
-let exp = await pr.experiment("exp001")
-let ds = pr.dataset("iris").batch(150)
-                           .repeat(1000)
-
-for (const batch of ds) {
-  let { features, labels } = await batch
-  exp.sgd({ lr: 0.01 }, (params) =>
-    features
-    .linear("L1", params, 4).relu()
-    .linear("L2", params, 3)
-    .softmaxLoss(labels))
-  // Delete to train.
-  break;
-}
-`;
-
-const TrainingExample = () => (
-  <div class="training">
-    <div class="training__inner">
-      <h2 class="training__header">Neural Networks</h2>
-      <div class="flex-row">
-        <div class="flex-cell training__example">
-          { nb.cell(trainingExampleCode) }
-        </div>
-        <div class="flex-cell">
-          <p class="training__copy">
-            <b>What are NNs anyway?</b>
-          </p>
-          <p class="training__copy">
-            The terminology is a bit misleading. Any
-            number of operations and architectures can be considered a neural
-            network. The primary thing in common is that NN models use
-            differentiable operations to allow gradient descent to improve their
-            fitness.
-          </p>
-          <p class="training__copy">
-            Propel provides a concise API for specifying, training, and
-            making inference in neural networks. In the example, a two layer
-            fully-connected ReLU network is being trained on the iris dataset.
-            Iris is a very small dataset where each training example is only
-            4 floating point features. There are three possible labels to apply.
-            As with all classification problems, we apply softmaxLoss to the
-            labels.
-          </p>
-        </div>
-      </div>
-    </div>
-  </div>
-);
-
-function fixed(code: string): JSX.Element {
-  return h(nb.FixedCell, { code });
-}
-
-function headerButton(href, text) {
-  return <a class="button header-button" href={ href }>{ text }</a>;
-}
